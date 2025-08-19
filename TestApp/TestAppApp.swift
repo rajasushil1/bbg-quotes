8bdd01a56092d03304758abedbd921b1345db349
Sushil Adhikari <sushil@yarsa.io>
2025-08-19 10:19:01 +0545

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Raja Sushil Adhikari on 19/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
