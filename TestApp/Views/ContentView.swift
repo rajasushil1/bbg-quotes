8a64f7dd6f9c0407d5d8d665ef4257b33312ed0d
Sushil Adhikari <sushil@yarsa.io>
2025-08-19 16:20:58 +0545

    tiktok view completed

---
+++ TestApp/Views/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Raja Sushil Adhikari on 19/08/2025.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TikTokQuotesFeed(quotes: Quote.samples)
+    }
+}
+
+#Preview {
+    ContentView()
+}
--- TestApp/Views/MainPage.swift
+++ TestApp/Views/MainPage.swift
@@ -4,4 +4,41 @@
 //
 //  Created by Raja Sushil Adhikari on 19/08/2025.
 //
+import SwiftUI
+import UIKit
 
+struct MainView: View {
+    init() {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = UIColor.black
+            appearance.stackedLayoutAppearance.selected.iconColor = .white
+            appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.stackedLayoutAppearance.normal.iconColor = .gray
+            appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.gray]
+            UITabBar.appearance().standardAppearance = appearance
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+           
+        }
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+                
+
+            FeedPage()
+                .tabItem {
+                    Label("Order", systemImage: "square.and.pencil")
+                }
+               
+            SettingsPage()
+                .tabItem {
+                    Label("Settings", systemImage: "gearshape")
+                }
+               
+        }
+        .accentColor(.white)
+    }
+}
